85dceca845fb2131541f928b5aedc0a6248d7322
Draakvoid <Draakvoid@gmail.com>
2024-06-12 22:25:36 -0400

    revert back to greatness

--- ui/v2.5/src/components/Performers/PerformerDetails/Performer.tsx
+++ ui/v2.5/src/components/Performers/PerformerDetails/Performer.tsx
@@ -242,7 +242,11 @@ const PerformerPage: React.FC<IProps> = ({ performer, tabKey }) => {
           }
       }
   }})
-  
+  useEffect(() => {
+    if (data?.findImages.count != 0) {
+      setAltImage(data?.findImages.images[0].paths.image ?? null);
+    }
+  }, [data]);
   return data?.findImages.count != 0 ? <img className="alt-hidden" src={data?.findImages.images[0].paths.image ?? ""}/> : ""
   }
 
